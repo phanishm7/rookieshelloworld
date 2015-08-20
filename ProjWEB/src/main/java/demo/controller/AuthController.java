977ce12ac18c17a2a0068b00d79accd88616d89c
Cloud User <ec2-user@ip-172-31-56-255.ec2.internal>
2015-08-19 22:31:13 -0400

    m71

--- ProjWEB/src/main/java/demo/controller/AuthController.java
+++ ProjWEB/src/main/java/demo/controller/AuthController.java
@@ -32,7 +32,7 @@ public class AuthController {
 
         ResultSet results = session.execute("select * from xmbl_gfcd_acct where mbl_num_i='"+phone+"'");
 
-        System.err.println("m7");
+        System.err.println("m71");
 
         String enryptedDbPin ="";
 

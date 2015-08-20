250d04b403f5b531f1f98c1468db7e15d92d80ad
Cloud User <ec2-user@ip-172-31-56-255.ec2.internal>
2015-08-19 21:22:09 -0400

    from 101

--- ProjWEB/src/main/java/demo/controller/AuthController.java
+++ ProjWEB/src/main/java/demo/controller/AuthController.java
@@ -31,7 +31,7 @@ public class AuthController {
         Session session = cluster.connect("demo");
 
         ResultSet results = session.execute("select * from xmbl_gfcd_acct where mbl_num_i='"+phone+"'");
-        System.err.println("1"); 
+        System.err.println("from101"); 
         String enryptedDbPin ="";
 
         //get the encrypted pin
